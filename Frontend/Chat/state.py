6bb94945d9b520c471d39ec16633313b78e8c78a
vikas-gits-good <vikas.c.conappa@protonmail.com>
2026-02-26 16:14:08 +0530

    FE - added message list to state

--- Frontend/Chat/state.py
+++ Frontend/Chat/state.py
@@ -3,17 +3,35 @@ import asyncio
 import reflex as rx
 
 
+class ChatMessage(rx.Base):
+    message: str
+    is_bot: bool = False
+
+
 class ChatState(rx.State):
     DID_SUBMT: bool = False
+    MESSAGES: list[ChatMessage] = []
 
     @rx.var
     def user_form_submit(self) -> bool:
         return self.DID_SUBMT
 
     async def handle_submit(self, form_data: dict = {}):
-        self.DID_SUBMT = True
-        yield
+        user_message = form_data.get("HumanMessage", "")
+        if user_message:
+            self.DID_SUBMT = True
+            self._append_message(user_message, False)
+            yield
 
-        await asyncio.sleep(4)
-        self.DID_SUBMT = False
-        yield
+            await asyncio.sleep(4)
+            self.DID_SUBMT = False
+            self._append_message(user_message, True)
+            yield
+
+    def _append_message(self, message, is_bot: bool = False):
+        self.MESSAGES.append(
+            ChatMessage(
+                message=message,
+                is_bot=is_bot,
+            )
+        )
