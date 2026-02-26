3c13e9c756f87099ede40fc4d7f37b4fd8fb2f3a
vikas-gits-good <vikas.c.conappa@protonmail.com>
2026-02-26 13:53:10 +0530

    FE- initial webpage

--- Frontend/Frontend.py
+++ Frontend/Frontend.py
@@ -1,1 +1,55 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
 import reflex as rx
+
+from rxconfig import config
+
+
+class State(rx.State):
+    """The app state."""
+
+
+def about() -> rx.Component:
+    # About Page (about)
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading("Welcome to Reflex about!", size="9"),
+            rx.text(
+                "Get started by editing ",
+                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                size="5",
+            ),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
+        ),
+    )
+
+
+def home() -> rx.Component:
+    # Welcome Page (home)
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading("Welcome to Reflex GPT!", size="9"),
+            rx.text(
+                "Get started by editing ",
+                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                size="5",
+            ),
+            rx.link(
+                rx.button("Check out our docs!"),
+                href="https://reflex.dev/docs/getting-started/introduction/",
+                is_external=True,
+            ),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
+        ),
+    )
+
+
+app = rx.App()
+app.add_page(home, route="/")
+app.add_page(about, route="/about")
