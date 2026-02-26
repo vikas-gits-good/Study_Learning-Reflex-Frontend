ab5a4782e37607844ceb7c6dcd3ff04641833458
vikas-gits-good <vikas.c.conappa@protonmail.com>
2026-02-26 16:01:43 +0530

    FE- added form submission state

--- Frontend/Chat/form.py
+++ Frontend/Chat/form.py
@@ -12,9 +12,20 @@ def chat_form() -> rx.Component:
                 required=True,
                 width="100%",
             ),
-            rx.button(
-                "Submit",
-                type="submit",
+            rx.hstack(
+                rx.button(
+                    "Submit",
+                    type="submit",
+                    id="chat-button-submit",
+                ),
+                rx.cond(
+                    ChatState.user_form_submit,
+                    rx.text(
+                        "Submitted.",
+                        size="3",
+                    ),
+                    rx.fragment(),
+                ),
             ),
         ),
         on_submit=ChatState.handle_submit,
--- Frontend/Chat/state.py
+++ Frontend/Chat/state.py
@@ -2,5 +2,11 @@ import reflex as rx
 
 
 class ChatState(rx.State):
+    DID_SUBMT: bool = False
+
+    @rx.var
+    def user_form_submit(self) -> bool:
+        return self.DID_SUBMT
+
     def handle_submit(self, form_data: dict = {}):
-        print(f"{form_data = }")
+        self.DID_SUBMT = True
